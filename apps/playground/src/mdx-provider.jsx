80a32af4c3dec6b41e3dc9825dec6e642c311612
moyarich <moyarich@hotmail.com>
2026-09-25 16:25:47 -0500

    docs: expose colorshower to page.mdx

--- apps/playground/src/mdx-provider.jsx
+++ apps/playground/src/mdx-provider.jsx
@@ -1,5 +1,6 @@
 import { MDXProvider } from "@mdx-js/react";
 import { Console, Terminal } from "@moyarich/console";
+import * as ColorShower from "@moyarich/vscode-visualize-css-colors";
 
 function Demo({ children }) {
   return <section data-demo>{children}</section>;
@@ -12,6 +13,7 @@ function PlaygroundCallout({ children }) {
 const components = {
   Console,
   Terminal,
+  ColorShower,
   Demo,
   Callout: PlaygroundCallout,
 };
