209fc42953dba5d2b351b900443c947c7cfba1df
moyarich <moyarich@hotmail.com>
2026-09-25 14:18:06 -0500

    feat: expose MoyaForge MDX defaults

---
+++ packages/moyaforge/src/components.js
@@ -0,0 +1,7 @@
+/**
+ * Built-in MDX components provided by MoyaForge.
+ *
+ * This object is intentionally plain so consumers can spread it into the
+ * standard MDX provider component map and override any key.
+ */
+export const moyaForgeComponents = Object.freeze({});
